4229603e698b7f5fb1b18faa0b2c001513f39e61
EEstaniel <edwineestaniel93@gmail.com>
2023-11-07 03:40:14 -0800

    Close popup after selecting

--- src/app/dashboard/invoices/components/SheetView.jsx
+++ src/app/dashboard/invoices/components/SheetView.jsx
@@ -1,8 +1,15 @@
 "use client"
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import {Input} from "@/@/components/ui/input"
 import {Label} from "@/@/components/ui/label"
 import {Button} from "@/@/components/ui/button";
+import {Calendar} from "@/@/components/ui/calendar"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/@/components/ui/popover"
+import {format} from 'date-fns';
 
 
 export default function SheetView({setSheetOpen}) {
@@ -22,13 +29,23 @@ export default function SheetView({setSheetOpen}) {
             country: ''
         },
         invoiceDetails: {
-            invoiceDate: new Date(),
+            invoiceDate: new Date().toString(),
             paymentTerms: 'Net 30 Days',
             projectDescription: ''
         },
         itemList: []
 
     });
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+    const [isTermsPopoverOpen, setIsTermsPopoverOpen] = useState(false);
+    const formRef = useRef(null);
+
+    const focusForm = () => {
+        if (formRef.current) {
+            formRef.current.focus(); // Brings the form into focus
+        }
+    };
 
     const addItem = () => {
         const newItem = {name: "", quantity: "", price: "", total: ""};
@@ -97,7 +114,9 @@ export default function SheetView({setSheetOpen}) {
     return (
         <>
             {/* Bill from group */}
-            <div className="pl-[10.5rem] pr-[3.5rem] w-full] overflow-auto mt-10 flex flex-col flex-grow h-screen">
+            <div id="thisForm"
+                 ref={formRef}
+                 className="pl-[10.5rem] pr-[3.5rem] w-full] overflow-auto mt-10 flex flex-col flex-grow h-screen">
                 <h1 className="heading-m">New Invoice</h1>
 
                 {/* Bill From*/}
@@ -198,40 +217,104 @@ export default function SheetView({setSheetOpen}) {
                 {/* Invoice Details */}
                 <div className="bill-to-group flex flex-col gap-[1.5rem] mb-[1.5rem] mt-[3rem]">
                     <div className="invoice-date-term-group flex flex-row gap-[1.5rem]">
-                        <div className="group flex flex-col w-full gap-[0.625rem] relative ">
-                            <Label htmlFor="to_country" className="body-v text-7-info">Invoice Date</Label>
-                            <Input
-                                className="w-full heading-s-v text-8-text opacity-[50%] pl-[1.25rem] border-5-secondary justify-between"
-                                value={invoiceData.invoiceDetails.invoiceDate}
-                                onChange={e => handleChange('invoiceDetails', 'invoiceDate', e.target.value)}
-                            />
-                            <div
-                                className="data-group flex flex-row justify-between pl-[1.25rem] pr-[1rem] absolute h-full top-[60%] gap-[6rem]">
-                                <p className="heading-s-v opacity-50 ">{invoiceData['invoiceDetails']['invoiceDate'].toString()}</p>
-                                <svg className=""
-                                     width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M14 2h-.667V.667A.667.667 0 0012.667 0H12a.667.667 0 00-.667.667V2H4.667V.667A.667.667 0 004 0h-.667a.667.667 0 00-.666.667V2H2C.897 2 0 2.897 0 4v10c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zm.667 12c0 .367-.3.667-.667.667H2A.668.668 0 011.333 14V6.693h13.334V14z"
-                                        fill="#7E88C3" fillRule="nonzero" opacity=".5"/>
-                                </svg>
-                            </div>
+                        <div className="group flex flex-col h-full w-full gap-[0.625rem] relative ">
+                            <Label htmlFor="invoice_date" className="body-v text-7-info">Invoice Date</Label>
+                            <Popover isOpen={isPopoverOpen} onOpen={() => setIsPopoverOpen(true)}
+                                     onClose={() => setIsPopoverOpen(false)}>
+                                <PopoverTrigger asChild>
+                                    <Button variant={"outline"}
+                                            className={("w-full justify-start text-left heading-s-v opacity-50 group-hover:cursor-pointer group-hover:border-1-primary group-hover:opacity-100")}>
+                                        {selectedDate && !isNaN(selectedDate.getTime())
+                                            ? format(selectedDate, "dd MMM yyyy")
+                                            : <span>Pick a date</span>
+                                        }
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-auto p-0" align="start">
+                                    <Calendar
+                                        mode="single"
+                                        selected={selectedDate}
+                                        onSelect={(date) => {
+                                            if (date) {
+                                                setSelectedDate(date);
+                                                handleChange('invoiceDetails', 'invoiceDate', format(date, "dd MMM yyyy"));
+                                            }
+                                            setIsPopoverOpen(false);
+                                            focusForm()
+                                        }}
+                                        initialFocus
+                                    />
+                                </PopoverContent>
+                            </Popover>
                         </div>
+
+                        {/* payment Terms*/}
                         <div className="group flex flex-col w-full gap-[0.625rem] relative">
-                            <Label htmlFor="to_country" className="body-v text-7-info">Payment Terms</Label>
-                            <Input
-                                className="w-full heading-s-v text-8-text opacity-[50%] pl-[1.25rem] border-5-secondary"
-                                value={invoiceData.invoiceDetails.paymentTerms}
-                                onChange={e => handleChange('invoiceDetails', 'paymentTerms', e.target.value)}
-                            />
-                            <div
-                                className="data-group flex flex-row justify-between pl-[1.25rem] pr-[1rem] absolute h-full top-[60%] gap-[6rem]">
-                                <p className="heading-s-v ">Net 30 Days</p>
-                                <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 10">
-                                    <path d="M1 1l4.228 4.228L9.456 1" stroke="#7C5DFA" strokeWidth="2" fill="none"
-                                          fillRule="evenodd"/>
-                                </svg>
-                            </div>
+                            <Label htmlFor="payment_terms" className="body-v text-7-info">Payment Terms</Label>
+                            <Popover isOpen={isTermsPopoverOpen} onOpen={() => setIsTermsPopoverOpen(true)}
+                                     onClose={() => setIsTermsPopoverOpen(false)}>
+                                <PopoverTrigger asChild>
+                                    <div className="flex justify-between items-center hover:cursor-pointer group">
+                                        <Input
+                                            id="payment_terms"
+                                            className="w-full heading-s-v text-8-text opacity-[50%] pl-[1.25rem] border-5-secondary group-hover:border-1-primary group-hover:opacity-100
+                                            group-hover:cursor-pointer"
+                                            value={invoiceData.invoiceDetails.paymentTerms}
+                                            // onclick set oposite value of isTermsPopoverOpen
+                                            onClick={() => setIsTermsPopoverOpen(!isTermsPopoverOpen)}
+                                            onChange={e => handleChange('invoiceDetails', 'paymentTerms', e.target.value)}
+                                        />
+                                        <div className="svg absolute right-4">
+                                            {isTermsPopoverOpen ? (
+                                                <svg width="10" height="7" viewBox="0 0 10 7" fill="none"
+                                                     xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M1 6.22803L5.2279 2.00013L9.4558 6.22803" stroke="#7C5DFA"
+                                                          strokeWidth="2" fill="none"/>
+                                                </svg>
+                                            ) : (
+                                                <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg"
+                                                     viewBox="0 0 15 10">
+                                                    <path d="M1 1l4.228 4.228L9.456 1" stroke="#7C5DFA" strokeWidth="2"
+                                                          fill="none" fillRule="evenodd"/>
+                                                </svg>
+                                            )}
+                                        </div>
+                                    </div>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-[15rem] p-0 ">
+                                    {/* Popover content */}
+                                    <div className="w-full group">
+                                        <p className="heading-s-v text-8-text w-full pl-6 py-4 group-hover:cursor-pointer group-hover:text-1-primary"
+                                           onClick={() => {
+                                               handleChange('invoiceDetails', 'paymentTerms', 'Net 1 Day');
+                                               setIsTermsPopoverOpen(!isTermsPopoverOpen)
+                                               focusForm()
+                                           }}
+                                        >Net 1 Day
+                                        </p>
+                                        <hr/>
+                                    </div>
+                                    <div className="w-full group">
+                                        <p className="heading-s-v text-8-text w-full pl-6 my-4 group-hover:cursor-pointer group-hover:text-1-primary"
+                                           onClick={() => handleChange('invoiceDetails', 'paymentTerms', 'Net 7 Day')}
+                                        >Net 7 Day</p>
+                                        <hr/>
+                                    </div>
+                                    <div className="w-full group">
+                                        <p className="heading-s-v text-8-text w-full pl-6 my-4 group-hover:cursor-pointer group-hover:text-1-primary"
+                                           onClick={() => handleChange('invoiceDetails', 'paymentTerms', 'Net 14 Day')}
+                                        >Net 14 Day</p>
+                                        <hr/>
+                                    </div>
+                                    <div className="w-full group">
+                                        <p className="heading-s-v text-8-text w-full pl-6 my-4 group-hover:cursor-pointer group-hover:text-1-primary"
+                                           onClick={() => handleChange('invoiceDetails', 'paymentTerms', 'Net 30 Day')}
+                                        >Net 30 Day</p>
+                                    </div>
+                                </PopoverContent>
+                            </Popover>
                         </div>
+
                     </div>
                     <div className="group flex flex-col w-full gap-[0.625rem]">
                         <Label htmlFor="to_country" className="body-v text-7-info">Project Description</Label>
