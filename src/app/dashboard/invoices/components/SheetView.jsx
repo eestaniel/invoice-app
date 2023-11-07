487e823aa52e7868640d28d44da071db0ddbc0c4
EEstaniel <edwineestaniel93@gmail.com>
2023-11-07 03:57:25 -0800

    Change delete png to svg and hover states

--- src/app/dashboard/invoices/components/SheetView.jsx
+++ src/app/dashboard/invoices/components/SheetView.jsx
@@ -378,9 +378,11 @@ export default function SheetView({setSheetOpen}) {
                                 />
                                 <p className="grow  h-full heading-s-v text-6-muted pl-2">{item.quantity * item.price}</p>
                                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                                <img src="/delete.png" alt="delete icon" className="w-[1rem] h-[1rem]"
-                                     onClick={() => removeItem(index)}
-                                />
+                                <svg width="13" height="16" xmlns="http://www.w3.org/2000/svg" onClick={()=> removeItem(index)}>
+                                    <path className="hover:cursor-pointer hover:fill-red-500"
+                                        d="M11.583 3.556v10.666c0 .982-.795 1.778-1.777 1.778H2.694a1.777 1.777 0 01-1.777-1.778V3.556h10.666zM8.473 0l.888.889h3.111v1.778H.028V.889h3.11L4.029 0h4.444z"
+                                        fill="#888EB0" fillRule="nonzero"/>
+                                </svg>
                             </div>
                         ))}
                     </div>
