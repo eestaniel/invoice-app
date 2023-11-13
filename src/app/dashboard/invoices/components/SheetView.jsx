78fb920f10bc7718729a811b268fbf2b598480bb
EEstaniel <edwineestaniel93@gmail.com>
2023-11-13 02:52:02 -0800

    Fix discard new sheet

--- src/app/dashboard/invoices/components/SheetView.jsx
+++ src/app/dashboard/invoices/components/SheetView.jsx
@@ -447,14 +447,12 @@ export default function SheetView({setInvoiceOptions, sheetType, data, sheetMeth
             <div className="group-buttons flex flex-row justify-between gap-[0.5rem] mt-[3rem]  w-full
       h-[6.875rem] grow p-[1.5rem]"
             >
-              <Button
-                className="w-[6rem] h-[3rem] rounded-[1.5rem] bg-[#F9FAFE] text-7-info heading-s-v stick hover:bg-9-accent hover:text-8-text"
-                // on click discard all changes and close sheet
-                onClick={() => setSheetOpen(false)}
-
-              >
-                Discard
-              </Button>
+              <SheetClose asChild>
+                <Button
+                  className="w-[6rem] h-[3rem] rounded-[1.5rem] bg-[#F9FAFE] text-7-info heading-s-v stick hover:bg-9-accent hover:text-8-text">
+                  Discard
+                </Button>
+              </SheetClose>
               <div className="groupbuttons flex flex-row gap-[0.5rem]">
                 <SheetClose asChild>
                   <Button className="w-[8.25rem] h-[3rem] rounded-[1.5rem] bg-[#373B53] heading-s-v text-6-muted
@@ -487,26 +485,27 @@ export default function SheetView({setInvoiceOptions, sheetType, data, sheetMeth
                   </Button>
                 </SheetClose>
                 <SheetClose asChild>
-                  <Button className="w-[8rem] h-[3rem] rounded-[1.5rem] bg-1-primary heading-s-v text-white hover:cursor-pointer hover:bg-2-highlight"
-                          onClick={(e) => {
-                            const form = e.target.form
-                            const method = form.getAttribute('method').toUpperCase()
-                            const action = form.getAttribute('action')
+                  <Button
+                    className="w-[8rem] h-[3rem] rounded-[1.5rem] bg-1-primary heading-s-v text-white hover:cursor-pointer hover:bg-2-highlight"
+                    onClick={(e) => {
+                      const form = e.target.form
+                      const method = form.getAttribute('method').toUpperCase()
+                      const action = form.getAttribute('action')
 
 
-                            setInvoiceOptions(prev => {
-                              return {
-                                ...prev,
-                                action: action,
-                                body: invoiceData,
-                                method: method,
-                                invoiceData: invoiceData,
-                                type: 'create',
-                                status: 'pending'
-                              }
-                            })
+                      setInvoiceOptions(prev => {
+                        return {
+                          ...prev,
+                          action: action,
+                          body: invoiceData,
+                          method: method,
+                          invoiceData: invoiceData,
+                          type: 'create',
+                          status: 'pending'
+                        }
+                      })
 
-                          }}
+                    }}
 
                   >Save & Send
                   </Button>
